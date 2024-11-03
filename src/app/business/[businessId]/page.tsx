2b0d5608541287f810008a5d7af17b12cbe9ddad
Isaque Delfino <isa.quecosta00@gmail.com>
2024-11-03 20:40:20 +0000

    initial commit

---
+++ src/app/business/[businessId]/page.tsx
@@ -0,0 +1,13 @@
+// businessId/page.tsx
+
+import { Input } from '@/components/ui/input'
+
+export default function BusinessPage() {
+  return (
+    <main>
+      <form>
+        <Input type="text" placeholder={`Procure por "Bifana"`} />
+      </form>
+    </main>
+  )
+}
